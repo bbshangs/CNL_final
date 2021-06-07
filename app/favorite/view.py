304e4e314a4713444f3e0aab1655dc1a056ee749
Sophia Huang <b07902031@csie.ntu.edu.tw>
2021-06-07 22:56:29 +0800

    add favorite method

--- app/favorite/view.py
+++ app/favorite/view.py
@@ -1,6 +1,8 @@
 from app import app
 from flask import render_template
+from app.user.model import UserRegister
 
 @app.route('/favorite')  
 def favorite():  
     return render_template('favorite/favorite.html')
+
--- app/user/model.py
+++ app/user/model.py
@@ -6,11 +6,18 @@ class UserRegister(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(50), nullable=False)
+    favorite = db.Column(db.String(27*10))
 
     def check_password(self, password):
         if self.password == password:
             return True
         return False
+
+    def add_favorite(self, place_id):
+        self.favorite += place_id
+
+    def get_favorite():
+        return favorite
 
     def __repr__(self):
         return 'username:%s' % self.username
