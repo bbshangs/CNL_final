f97fc404253e16dfd093ea8d5e259e95d54dd947
yui1910 <b07902033@ntu.edu.tw>
2021-06-11 01:07:21 +0800

    print favorite restaurant

--- app/favorite/view.py
+++ app/favorite/view.py
@@ -1,15 +1,17 @@
 from app import app
 from flask import render_template
 from app.user.model import UserRegister
+from app.restaurant.model import Restaurant
 
 @app.route('/favorite/<user_id>', methods=['GET', 'POST']) 
 def favorite(user_id):
     user = UserRegister.query.filter_by(id=user_id).first()
-    f = user.get_favorite()
-    print("favorite = ", f)
-    
-    favorite_list = []
-    favorite_list.append(f)
+    favorite_list = user.get_favorite().split()
 
-    return render_template('favorite/favorite.html', user_id=user_id, favorite_list=favorite_list)
+    restaurant_list = []
+    for favorite in favorite_list:
+        restaurant = Restaurant.query.filter_by(place_id=favorite).first()
+        restaurant_list.append(restaurant)                                
+    return render_template('favorite/favorite.html',
+                user_id=user_id, restaurant_list=restaurant_list)
 
