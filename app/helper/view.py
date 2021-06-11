57671797d902a81365c65aebf524551aab402736
Sophia Huang <b07902031@csie.ntu.edu.tw>
2021-06-11 14:38:29 +0800

    add the helper template but still with some bugs

--- app/helper/view.py
+++ app/helper/view.py
@@ -1,6 +1,8 @@
 from app import app
 from flask import render_template
+from app.user.model import UserRegister
 
-@app.route('/helper')  
-def helper():  
-    return render_template('helper/helper.html')
+@app.route('/helper/<user_id>', methods=['GET', 'POST'])  
+def helper(user_id):  
+	user = UserRegister.query.filter_by(id=user_id).first()
+	return render_template('helper/helper.html', user_id=user_id)
